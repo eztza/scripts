a36a43048dbddc01b32e57159388ec2f24d97555
Ezat Za <65310602+eztza@users.noreply.github.com>
2020-12-29 15:37:24 +0200

    Add files via upload

---
+++ backtotop.js
@@ -0,0 +1,19 @@
+// by Ezat Zaino
+// back to top
+mybutton = document.getElementById("backtotop");
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
