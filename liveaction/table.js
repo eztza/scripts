b9c43054d4538b8aa207e589169f58c8ad8e8265
Ezat Zaino <65310602+eztza@users.noreply.github.com>
2021-01-10 16:44:43 +0200

    Update table.js

--- liveaction/table.js
+++ liveaction/table.js
@@ -1,1 +1,40 @@
-console.log('Hello World!')
+document.getElementById("table").innerHTML = `
+<div class="liveMatches mb20"><h3>
+  <a>أهم المباريات 2/10/2020</a></h3>
+  
+  <table width="100%" cellspacing="0" cellpadding="0" class="cellCenter"><tbody>
+    
+
+     <tr class="matchRow" ><td class="liveTeam">
+       
+       <img alt="mnajiri" src="https://ssl.gstatic.com/onebox/media/sports/logos/Th4fAVAZeCJWRcKoLW7koA_48x48.png"><div>ريال مدريد</div></td>
+       
+       <td class="liveDet"><div class="liveVersus">لم تحدد<br><span class="bgShare"></span>00:00م</div></td><td class="liveTeam">
+       
+       <img alt="mnajiri" src="https://ssl.gstatic.com/onebox/media/sports/logos/paYnEE8hcrP96neHRNofhQ_48x48.png"><div>برشلونة</div></td></tr>   
+    
+        </tbody>
+</table>
+</div>
+
+<!-- End Match -->
+	
+<!-- Start Match -->
+		
+<div class="liveMatches mb20">
+  
+  <table width="100%" cellspacing="0" cellpadding="0" class="cellCenter"><tbody>
+    
+
+     <tr class="matchRow" ><td class="liveTeam">
+       
+       <img alt="mnajiri" src="https://ssl.gstatic.com/onebox/media/sports/logos/Lv6xmBlUIpN3GAFhtf6nqQ_48x48.png"><div>يوفينتوس</div></td>
+       
+       <td class="liveDet"><div class="liveVersus">لم تحدد<br><span class="bgShare"></span>00:00م</div></td><td class="liveTeam">
+       
+       <img alt="mnajiri" src="https://ssl.gstatic.com/onebox/media/sports/logos/srAAE0bOnCppUrlbJpFiHQ_48x48.png"><div>اتلتيكو</div></td></tr>   
+    
+        </tbody>
+</table>
+</div>
+`
